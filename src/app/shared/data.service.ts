929f24e8f6fd7a602dc08a9f5b5df54577d87b97
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-09-07 14:39:31 +0300

    Reformat ...

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,54 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { retry, catchError } from 'rxjs/operators';
-import {  throwError, Observable } from 'rxjs';
-
+import { throwError, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DataService {
-  results: Object; 
-  private REST_API_SERVER = "http://104.208.220.28:3000";
+	results: Object;
+	private REST_API_SERVER = "http://104.208.220.28:3000";
 
-  constructor(private httpClient: HttpClient) { }
+	constructor(private httpClient: HttpClient) { }
 
+	handleError(error: HttpErrorResponse) {
+		let errorMessage = 'Unknown error!';
+		if (error.error instanceof ErrorEvent) {
+			// Client-side errors
+			errorMessage = `Error: ${error.error.message}`;
+		} else {
+			// Server-side errors
+			errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+		}
+		window.alert(errorMessage);
+		return throwError(errorMessage);
+	}
 
-  handleError(error: HttpErrorResponse) {
-    let errorMessage = 'Unknown error!';
-    if (error.error instanceof ErrorEvent) {
-      // Client-side errors
-      errorMessage = `Error: ${error.error.message}`;
-    } else {
-      // Server-side errors
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    window.alert(errorMessage);
-    return throwError(errorMessage);
-  }
+	sendPostRequest(data: object[]): Observable<any> {
+		return this.httpClient.post<any>(this.REST_API_SERVER, data).pipe(retry(3), catchError(this.handleError));
+	}
 
+	public setResuls(data: Object): Object {
+		this.results = data;
+		return data;
+	}
 
-    sendPostRequest(data: object[]): Observable<any> {
-      return this.httpClient.post<any>(this.REST_API_SERVER, data).pipe(retry(3), catchError(this.handleError));
- }
+	public getResults(): Object {
+		return this.results;
+	}
 
-
-
-  public setResuls(data: Object): Object {
-        this.results = data;
-        return data;
-  }
-
-
-  public getResults(): Object {
-    return  this.results;
-  }
-  
-  public deleteTempConfigFile(list: string[]): Observable<object>{
-    console.log("list:", list);
-    return this.httpClient.post<any>(`${this.REST_API_SERVER}/remove`, list).pipe(retry(3), catchError(this.handleError));
-  }
-
-
-
+	public deleteTempConfigFile(list: string[]): Observable<object> {
+		console.log("list:", list);
+		return this.httpClient.post<any>(`${this.REST_API_SERVER}/remove`, list).pipe(retry(3), catchError(this.handleError));
+	}
 }
