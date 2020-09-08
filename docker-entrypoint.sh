6b4c3ee2289d92bc99eeaf3eedd0fee27178aaa0
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-09-08 12:43:49 +0300

    Added initial support for env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 echo "Starting application..."
 echo "HOST_IP = ${HOST_IP}"
 envsubst < "/usr/share/nginx/html/assets/json/runtime.json" > "/usr/share/nginx/html/assets/json/runtime.json"
