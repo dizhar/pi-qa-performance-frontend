6f65844a97fcb71fc889d47ed6a780328e7e3108
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-09-08 13:42:08 +0300

    Added support for env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Starting application..."
-echo "HOST_IP = ${HOST_IP}"
-envsubst < "/usr/share/nginx/html/assets/json/runtime.json" > "/usr/share/nginx/html/assets/json/runtime.json"
+echo "Starting pi-qa-performance-app fronend ..."
+
+export BACKEND_IP=${HOST_IP}
+export BACKEND_PORT=${BACKEND_PORT:-"3000"}
+
+echo "HOST_IP       : ${HOST_IP}"
+echo "BACKEND_IP    : ${BACKEND_IP}"
+echo "BACKEND_PORT  : ${BACKEND_PORT}"
+
+# envsubst < "/usr/share/nginx/html/assets/json/runtime.json" > "/usr/share/nginx/html/assets/json/runtime.json"
+envsubst '${BACKEND_IP},${BACKEND_PORT}' <"/usr/share/nginx/html/assets/json/runtime.json"
+
 nginx -g 'daemon off;'
